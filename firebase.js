6872a8b97ba24d71b0dbe56d0b5bce2b92400c96
John <jhvoong@ucdavis.edu>
2020-10-13 01:20:08 -0700

    Initial

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAS3NRr4rJisWBU1Xm5n6iaS7unYuBv1Sk",
+  authDomain: "facebook-clone-5c18f.firebaseapp.com",
+  databaseURL: "https://facebook-clone-5c18f.firebaseio.com",
+  projectId: "facebook-clone-5c18f",
+  storageBucket: "facebook-clone-5c18f.appspot.com",
+  messagingSenderId: "373617668690",
+  appId: "1:373617668690:web:43612e74e5ec6faf57258b",
+  measurementId: "G-3DKZ2S18D3"
+};
---
+++ src/Header.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import "./Header.css";
+import AddIcon from "@material-ui/icons/Add"
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore"
+import FacebookIcon from "@material-ui/icons/Facebook"
+import FlagIcon from "@material-ui/icons/Flag"
+import ForumIcon from "@material-ui/icons/Forum"
+import HomeIcon from "@material-ui/icons/Home"
+import NotificationsActiveIcon from "@material-ui/icons/NotificationsActive"
+import SearchIcon from "@material-ui/icons/Search"
+import SubscriptionsOutlinedIcon from "@material-ui/icons/SubscriptionsOutlined"
+import StorefrontOutlinedIcon from "@material-ui/icons/StorefrontOutlined"
+import SupervisedUserCircleIcon from "@material-ui/icons/SupervisedUserCircle"
+
+import { Avatar, IconButton } from "@material-ui/core";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <FacebookIcon />
+        <div className="header__input">
+          <SearchIcon />
+          <input placeholder="Search Facebook" type="text" />
+        </div>
+      </div>
+
+      <div className="header__center">
+        <div className="header__option header__option--active">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontOutlinedIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
+      </div>
+
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>First Name</h4>
+        </div>
+      </div>
+
+      <IconButton>
+        <AddIcon />
+      </IconButton>
+      <IconButton>
+        <ForumIcon />
+      </IconButton>
+      <IconButton>
+        <NotificationsActiveIcon />
+      </IconButton>
+      <IconButton>
+        <ExpandMoreIcon />
+      </IconButton>
+    </div>
+  )
+}
+
+export default Header
